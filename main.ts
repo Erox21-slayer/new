11f979187dfefd2cd99c312601ef8831501e8fea
Erox21-slayer <82471801+Erox21-slayer@users.noreply.github.com>
2021-05-06 15:23:25 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,69 @@
+namespace SpriteKind {
+    export const target = SpriteKind.create()
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    ball = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . 2 2 . . . 
+        . . . . . . . . . . . 2 2 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, mySprite, -50, 0)
+    pause(200)
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.target, function (sprite, otherSprite) {
+    sprite.destroy()
+    info.changeScoreBy(1)
+})
+let ball: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
+    . . f f f . . . . . . . . . . . 
+    f f f c c . . . . . . . . f f f 
+    f f c c c . c c . . . f c b b c 
+    f f c 3 c c 3 c c f f b b b c . 
+    f f c 3 b c 3 b c f b b c c c . 
+    f c b b b b b b c f b c b c c . 
+    c c 1 b b b 1 b c b b c b b c . 
+    c b b b b b b b b b c c c b c . 
+    c b 1 f f 1 c b b c c c c c . . 
+    c f 1 f f 1 f b b b b f c . . . 
+    f f f f f f f b b b b f c . . . 
+    f f 2 2 2 2 f b b b b f c c . . 
+    . f 2 2 2 2 2 b b b c f . . . . 
+    . . f 2 2 2 b b b c f . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+mySprite.setPosition(145, 60)
+let mysprite2 = sprites.create(img`
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 2 2 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 2 2 2 2 1 1 1 1 1 1 
+    1 1 1 1 1 2 2 2 2 2 2 1 1 1 1 1 
+    1 1 1 1 1 1 2 2 2 2 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 2 2 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 
+    `, SpriteKind.target)
+mysprite2.setPosition(15, 60)
+info.startCountdown(10)
